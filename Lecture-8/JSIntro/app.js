55c8da7ca96fa1e1a5d6469b9bb539597ccaf837
Sabeel Khan <sabeelhps@gmail.com>
2021-08-04 18:19:14 +0530

    Lecture-8'

---
+++ Lecture-8/JSIntro/app.js
@@ -0,0 +1,16 @@
+// String template literals
+
+//This is a comment
+
+let firstName = 'Sabeel';
+let lastName = "Khan";
+
+// let fullName = "Mr." + lastName + "," + firstName;
+
+// Better way - using string template literals
+
+let fullName = `Mr.${lastName},${firstName}`;
+
+// console.log(fullName);
+
+
