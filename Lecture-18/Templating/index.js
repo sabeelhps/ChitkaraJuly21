464be2aeeb3a0b354c48cdb6c48ea5131d616a26
Sabeel Khan <sabeelhps@gmail.com>
2021-09-14 12:05:49 +0530

    Lecture-18

--- Lecture-18/Templating/index.js
+++ Lecture-18/Templating/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+
+const todos = ['Go to Market', 'Learn Web', 'Watch Movies', 'Play Football', 'Go Shopping'];
 
 
 app.get('/', (req, res) => {
@@ -9,6 +18,31 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/temp', (req, res) => {
+    
+    res.render("home");
+});
+
+app.get('/random', (req, res) => {
+    
+    const randomNum = Math.floor(Math.random() * 100)
+
+    res.render('random', { rnum: randomNum });
+});
+
+app.get('/todos', (req, res) => {
+    
+    res.render('todo', { todos });
+});
+
+app.get('/r/:subredit', (req, res) => {
+
+    const { subredit } = req.params;
+
+    // datbase logic
+
+    res.render('subreditDemo', { subredit });
+})
 
 
 app.listen(8080, () => {
---
+++ Lecture-18/Templating/public/js/script.js
@@ -0,0 +1,1 @@
+alert("Hello from Scripts.js file")
