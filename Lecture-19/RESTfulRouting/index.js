528f1d1146250789f9d7f915e621ccfaca633410
Sabeel Khan <sabeelhps@gmail.com>
2021-09-15 12:02:46 +0530

    Lecture-19

--- Lecture-19/RESTfulRouting/index.js
+++ Lecture-19/RESTfulRouting/index.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
+ 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
-const comments = [
+let comments = [
     {
-        id:0,
+        id:uuid(),
         username: 'Kartik',
         comment:'Hello from CB'
     },
     {
-        id:1,
+        id:uuid(),
         username: 'Cat',
         comment:'Meeooowww'
     },
     {
-        id:2,
+        id:uuid(),
         username: 'Dog',
         comment:'Woooof Wooof'
     },
@@ -51,7 +54,7 @@ app.get('/comments/new', (req, res) => {
 app.post('/comments', (req, res) => {
     
     const newComment = {
-        id:comments.length,
+        id:uuid(),
         ...req.body
     }
 
@@ -60,17 +63,52 @@ app.post('/comments', (req, res) => {
     res.redirect('/comments');
 });
 
-
+// Display one comment
 app.get('/comments/:id', (req, res) => {
    
     const { id } = req.params;
     
-    const foundComment = comments.find((c) => c.id === parseInt(id));
+    const foundComment = comments.find((c) => c.id === id);
 
     
     res.render('show', {foundComment });
 });
 
+// displaying the form with prefilled data to be edited
+app.get('/comments/:id/edit', (req, res) => {
+    
+    const { id } = req.params;
+
+    const foundComment = comments.find((c) => c.id === id);
+
+    res.render('edit', { foundComment });
+});
+
+// upadting the comment with the given id
+app.patch('/comments/:id', (req, res) => {
+   
+    const { id } = req.params;
+
+    const updatedCommentText = req.body.comment;
+
+    const foundComment = comments.find((c) => c.id === id);
+
+    foundComment.comment = updatedCommentText;
+
+    res.redirect('/comments');
+});
+
+// Delete and destroy particular route
+app.delete('/comments/:id', (req, res) => {
+    
+    const { id } = req.params;
+
+    const newCommentsArray = comments.filter((c) => c.id !== id);
+
+    comments = newCommentsArray;
+
+    res.redirect('/comments');
+});
 
 app.listen(2323, () => {
     console.log('server runnig at port 2323');
