3b5a256f8228f5da53ac3b552afd6926482ffbd6
Sabeel Khan <sabeelhps@gmail.com>
2021-10-07 12:30:53 +0530

    Lecture-28

--- Lecture-28/my-app/src/components/Person.js
+++ Lecture-28/my-app/src/components/Person.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import './Person.css';
 
 const Person = (props) => {
     
     // props.name = 'Anonymous'; ---> Props are immutable in react
 
-    return <article className="person">
-                <h2>Name : { props.name }</h2>
-                <h5>Age : { props.age}</h5>
-                <p>My Fav Color is : {props.favColor} </p>
-            </article>
+    const [name,setName] = useState(props.name);
+
+
+    const nameChangeHandler = (event) => {
+        setName('Anonymous');
+    }
+
+
+    return (
+        <article className="person">
+            <h2>Name : { name }</h2>
+            <h5>Age : { props.age}</h5>
+            <p>My Fav Color is : {props.favColor} </p>
+            <button onClick={nameChangeHandler}>Change Name</button>
+        </article>)
 
 }
 
--- Lecture-28/my-app/src/components/PersonList.js
+++ Lecture-28/my-app/src/components/PersonList.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Person from './Person'
 
 const PersonList = ()=> {
     
-    const persons = [
+    const [persons,setPersons] = useState([
         {
             name: 'Max',
             age: 25,
@@ -24,7 +24,7 @@ const PersonList = ()=> {
             age: 21,
             favColor: 'Purple'
         }
-    ];
+    ]);
 
     const personsList = persons.map((person,idx) => {
         
@@ -38,9 +38,72 @@ const PersonList = ()=> {
             
     });
 
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+    const [favColor, setFavColor] = useState('');
+
+
+    // const nameChangeHandler = (event) => {
+    //     setName(event.target.value);
+    // }
+
+    // const ageChangeHandler = (event) => {
+    //     setAge(event.target.value);
+    // }
+
+    // const colorChangeHandler = (event) => {
+    //     setFavColor(event.target.value);
+    // }
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        // const newPersons = [...persons];
+        // newPersons.push({ name, age, favColor });
+
+        // Better way of manipulating state
+        setPersons((prevState) => {
+            return [...prevState, { name, age, favColor }];
+        });
+   
+        // setPersons([...persons, { name, age, favColor }]);
+    }
+
     
     return (
         <div>
+            <form onSubmit={formSubmitHandler}>
+                <div>
+                    <label htmlFor="name">Name : </label>
+                    <input
+                        type="text"
+                        id="name"
+                        placeholder="Name"
+                        onChange={(e)=>setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="age">Age : </label>
+                    <input
+                        type="number"
+                        id="age"
+                        placeholder="Age"
+                        onChange={(e)=>setAge(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="color">Fav Color : </label>
+                    <input
+                        type="text"
+                        id="color"
+                        placeholder="Fav Color"
+                        onChange={(e)=>setFavColor(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            
+
            {personsList}
         </div>
     )
